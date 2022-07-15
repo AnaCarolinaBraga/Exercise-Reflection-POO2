a7f0aca17ca9940a73db4d12cc5c3239f6450098
AnaCarolinaBraga <anabraga95@gmail.com>
2022-07-14 21:38:03 -0300

    Commit inicial

---
+++ ExercicioPOO2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System.Reflection;
+
+namespace ExercicioPOO2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            CreateInstance();
+
+        }
+
+        public static void DisplayPublicProperties(Object obj)
+        {
+            foreach (var propertyInfo in obj.GetType().GetProperties())
+            {
+                var propertyName = propertyInfo.Name;
+                var propertyValue = propertyInfo.GetValue(obj);
+                Console.WriteLine($"{propertyName}={propertyValue}");
+            }
+        }
+
+        public static void CreateInstance()
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                if (type.IsAssignableTo(typeof(Student)) && type.IsClass && !type.IsAbstract)
+                {
+                    var instance = (Student)Activator.CreateInstance(type);
+                    var name = instance.Name;
+                    var university = instance.University;
+                    var number = instance.RollNumber;
+
+                    var operations = new Student(name, university, number);
+                    DisplayPublicProperties(operations);
+                }
+            }
+
+
+        }
+
+    }
+}
---
+++ ExercicioPOO2/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPOO2
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int RollNumber { get; set; }
+
+        public Student()
+        {
+
+        }
+        public Student(string Name, string University, int number)
+        {
+            Name = Name;
+            University = University;
+            RollNumber = number;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} - {University} - {RollNumber}");
+        }
+    }
+}
